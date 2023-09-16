f8f8055d9e2bbdcc4a2ad9e609e04004a9aa959d
Satoaki Ooto <satoaki.ooto@gmail.com>
2023-09-16 19:02:36 +0900

    add process to exclude scheme from urlhaus data

--- urlhaus.py
+++ urlhaus.py
@@ -1,28 +1,29 @@
+# Standard Library
+import re
 import urllib.request
 
 
-def urlhaus_list():
+def urlhaus_list(file_path: str) -> None:
     url = 'https://urlhaus.abuse.ch/downloads/text_online/'
-    filename = 'urlhaus.txt'
-    
-    with open(filename, 'w'):
-        pass
-    
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as res:
             body = res.read().decode('utf-8')
-            with open(filename, 'w') as f:
-                f.writelines(body)
+            pattern = r'\bhttps?:\/\/\b'
+            replacement = ''
+
+            with open(file_path, 'w') as f:
+                modified_contents = re.sub(pattern, replacement, body)
+                f.writelines(modified_contents)
     except urllib.error.HTTPError as err:
         print(err.code)
     except urllib.error.URLError as err:
         print(err.reason)
 
 
-def main():
-    # ここに処理を書く
-    urlhaus_list()
+def main() -> None:
+    file_path = 'urllist.txt'
+    urlhaus_list(file_path)
 
 
 if __name__ == "__main__":
