298670fbfcbdc5ccd9e5da1ec86a68e1766c8d23
Satoaki Ooto <satoaki.ooto@gmail.com>
2023-09-17 17:23:35 +0900

    refacotr script

--- urlhaus.py
+++ urlhaus.py
@@ -1,24 +1,37 @@
-# Standard Library
 import re
+import urllib.error
 import urllib.request
 
 
-def urlhaus_list(file_path: str) -> None:
+def urlhaus_list(output_file_path: str) -> None:
+    """
+    Downloads a text file from 'https://urlhaus.abuse.ch/downloads/text_online/'
+    and replaces all occurrences of 'http://' or 'https://' with an empty string.
+    The modified contents are then written to a file specified by 'output_file_path'.
+
+    Args:
+        output_file_path (str): The path to the file where the modified contents will be written.
+
+    Returns:
+        None
+    """
     url = 'https://urlhaus.abuse.ch/downloads/text_online/'
-    req = urllib.request.Request(url)
+
     try:
-        with urllib.request.urlopen(req) as res:
-            body = res.read().decode('utf-8')
-            pattern = r'\bhttps?:\/\/\b'
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            pattern = r'https?://'
             replacement = ''
 
-            with open(file_path, 'w') as f:
+            with open(output_file_path, 'w') as file:
                 modified_contents = re.sub(pattern, replacement, body)
-                f.writelines(modified_contents)
-    except urllib.error.HTTPError as err:
-        print(err.code)
-    except urllib.error.URLError as err:
-        print(err.reason)
+                file.write(modified_contents)
+    
+    except urllib.error.HTTPError as e:
+        print(e.code)
+    
+    except urllib.error.URLError as e:
+        print(e.reason)
 
 
 def main() -> None:
